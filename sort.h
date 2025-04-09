146e0e392a5ec3ad31d34efdc2ebede31b394d0e
Nada <Nada@MacBook-Pro-de-Macbook.local>
2025-04-10 00:49:45 +0200

    task0

--- sort.h
+++ sort.h
@@ -1,8 +1,7 @@
 #ifndef SORT_H
 #define SORT_H
 
-#include <stdio.h>
-#include <stdlib.h>
+#include <stddef.h>
 
 /**
  * struct listint_s - Doubly linked list node
@@ -13,12 +12,12 @@
  */
 typedef struct listint_s
 {
-	int n;
+	const int n;
 	struct listint_s *prev;
 	struct listint_s *next;
 } listint_t;
 
-/* Functions provided */
+/* Utility print functions */
 void print_array(const int *array, size_t size);
 void print_list(const listint_t *list);
 
